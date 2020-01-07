2a3956fa4f9e0755a2f9c8e27cb3595c2bc6f75e
Reyhan Azhari <reyhan_azhari@binusian.org>
2020-01-07 21:19:17 +0700

    Add Text Mesh Pro

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public int GameDuration = 30;
+
+    private int _counter = 0;
+
+    private Coroutine _coroutine;
+
+    public void StartGame()
+    {
+        _coroutine = StartCoroutine(GameEnumerator());
+    }
+
+    IEnumerator GameEnumerator()
+    {
+        while(_counter < GameDuration)
+        {
+            yield return new WaitForSeconds(1);
+            _counter++;
+        }
+    }
+}
